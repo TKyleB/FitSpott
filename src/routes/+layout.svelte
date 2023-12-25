943381b2db1ff82c97289249b27388fc73bca04e
TKyleB <kyle.beasley22@gmail.com>
2023-12-25 09:55:50 -0600

    Created layout & added bootstrap

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import "bootstrap/dist/css/bootstrap.min.css"
+    import scriptSrc from 'bootstrap/dist/js/bootstrap.bundle.js?url'
+    import Navbar from "$lib/Components/Navbar.svelte";
+</script>
+<svelte:head>
+    <script src="{scriptSrc}"></script>
+</svelte:head>
+<Navbar />
+<slot></slot>
