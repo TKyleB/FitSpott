920f43936194f9a7bb0e397ea52d1ff660701255
TKyleB <kyle.beasley22@gmail.com>
2023-12-25 09:56:06 -0600

    Basic navbar

---
+++ src/lib/Components/Navbar.svelte
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <!-- Brand/Logo -->
+        <a class="navbar-brand" href="/">Your Brand</a>
+
+        <!-- Collapse Button -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- Menu -->
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Register</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
